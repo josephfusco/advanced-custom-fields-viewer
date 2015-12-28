515d2f5213091b633725aaca9a5fe16fbd922940
Joseph Fusco <josephfusco@users.noreply.github.com>
2015-12-28 11:31:58 -0500

    Bump version.

--- acfv.php
+++ acfv.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Advanced Custom Fields Viewer
- * Version: 1.1.4
+ * Version: 1.1.5
  * Plugin URI: https://wordpress.org/plugins/advanced-custom-fields-viewer/
  * Description: View Advanced Custom Fields data on the front end of your theme while developing.
  * Author: Joseph Fusco
